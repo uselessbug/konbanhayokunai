fef3dac67ace60e8f3a73e14660cc0856a4a8e71
uselessbug <zhaodelin2002@live.com>
2023-04-06 12:30:22 +0800

    minor update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import requests
+# -*- coding: utf-8 -*-
+import datetime
 import json
+
+import requests
 
 ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;zfsoft'
 
 
 def login(xgh: str, password: str):
-    global ua
     url = "https://xssw.zjgsu.edu.cn/api/v1/login"
     data = {
         "xgh": xgh,
@@ -21,29 +23,31 @@ def login(xgh: str, password: str):
     return res.json()
 
 
-def get_list(token: str):
-    global ua
-    url = "https://xssw.zjgsu.edu.cn/counsellor/h5/app?path=pages/home/signin/index&_sid=" + token
+def get_cookies(access_token: str):
+    url = "https://xssw.zjgsu.edu.cn/counsellor/h5/app?path=pages/home/signin/index&_sid=" + access_token
     headers = {
         'User-Agent': ua
     }
-    '''print(url)'''
     s = requests.session()
     res = s.get(url=url, headers=headers, allow_redirects=False)
+    return res.cookies
+
+
+def get_list(first_access_token: str, type: str):
     url = "https://xssw.zjgsu.edu.cn/counsellor//api/v1/clockIn/task/getList"
     data = {
-        "today": True,
-        "history": False,
-        "future": False
+        type: True
     }
-    global authorizations
-    authorizations = headers['Authorization'] = "Bearer " + requests.utils.dict_from_cookiejar(res.cookies)['first_access_token']
+    headers = {
+        'User-Agent': ua,
+        'Authorization': "Bearer " + first_access_token
+    }
+    s = requests.session()
     res = s.post(url=url, headers=headers, json=data)
     return res.json()
 
 
-def signin(authorization: str, id: str, lon: float, la: float):
-    global ua
+def signin(first_access_token: str, id: str, lon: float, la: float):
     url = "https://xssw.zjgsu.edu.cn/counsellor//api/v1/clockIn/task/signin"
     data = {
         "id": id,
@@ -54,16 +58,19 @@ def signin(authorization: str, id: str, lon: float, la: float):
     }
     headers = {
         'User-Agent': ua,
-        'Authorization': authorization
+        'Authorization': "Bearer " + first_access_token
     }
     s = requests.session()
     res = s.post(url=url, headers=headers, json=data)
     return res.json()
 
+
 if __name__ == '__main__':
     with open('app-user.json', encoding='utf-8') as f:
         u = json.load(f)
-    t = login(u[0]["xgh"], u[0]["password"])['data']['access_token']
-    for i in get_list(t)['data']:
-        print(signin(authorizations, i['id'], u[0]["longitude"], u[0]["latitude"])['msg'])
-
+    at = login(u[0]["xgh"], u[0]["password"])['data']['access_token']
+    fat = requests.utils.dict_from_cookiejar(get_cookies(at))['first_access_token']
+    for i in get_list(fat, "history")['data']:
+        t = datetime.datetime.now().strftime('%Y-%m-%d')
+        m = signin(fat, i['id'], u[0]["longitude"], u[0]["latitude"])['msg']
+        print(t, "id: " + i['id'], m)
