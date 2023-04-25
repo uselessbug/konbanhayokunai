e8f7cc750fd2e10519b35b45226d3386986f0ea8
uselessbug <zhaodelin2002@live.com>
2023-04-25 13:06:55 +0800

    update

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import json
 import requests
 
 ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;zfsoft'
+null = None
+true = True
 
 
 def login(xgh: str, password: str):
-    url = "https://xssw.zjgsu.edu.cn/api/v1/login"
+    url = 'https://xssw.zjgsu.edu.cn/api/v1/login'
     data = {
-        "xgh": xgh,
-        "password": password,
-        "unionid": None
+        'xgh': xgh,
+        'password': password,
+        'unionid': null
     }
     headers = {
         'Content-Type': 'application/json;charset=UTF-8',
@@ -23,8 +25,8 @@ def login(xgh: str, password: str):
     return res.json()
 
 
-def get_cookies(access_token: str):
-    url = "https://xssw.zjgsu.edu.cn/counsellor/h5/app?path=pages/home/signin/index&_sid=" + access_token
+def getCookies(access_token: str):
+    url = 'https://xssw.zjgsu.edu.cn/counsellor/h5/app?path=pages/application/checkin/index&_sid=' + access_token
     headers = {
         'User-Agent': ua
     }
@@ -33,32 +35,31 @@ def get_cookies(access_token: str):
     return res.cookies
 
 
-def get_list(first_access_token: str, type: str):
-    url = "https://xssw.zjgsu.edu.cn/counsellor//api/v1/clockIn/task/getList"
+def getUserScheduling(first_access_token: str):
+    url = 'https://xssw.zjgsu.edu.cn/counsellor/api/v1/clockIn/scheduling/getUserScheduling'
     data = {
-        type: True
     }
     headers = {
         'User-Agent': ua,
-        'Authorization': "Bearer " + first_access_token
+        'Authorization': 'Bearer ' + first_access_token
     }
     s = requests.session()
     res = s.post(url=url, headers=headers, json=data)
     return res.json()
 
 
-def signin(first_access_token: str, id: str, lon: float, la: float):
-    url = "https://xssw.zjgsu.edu.cn/counsellor//api/v1/clockIn/task/signin"
+def userCheckIn(first_access_token: str, id: str, add: str, info: dict):
+    url = 'https://xssw.zjgsu.edu.cn/counsellor/api/v1/clockIn/scheduling/userCheckIn'
     data = {
-        "id": id,
-        "info": {
-            "longitude": lon,
-            "latitude": la
-        }
+        'address': add,
+        'word_id': id,
+        'info': info,
+        'exception_type': null,
+        'exceptions': null
     }
     headers = {
         'User-Agent': ua,
-        'Authorization': "Bearer " + first_access_token
+        'Authorization': 'Bearer ' + first_access_token
     }
     s = requests.session()
     res = s.post(url=url, headers=headers, json=data)
@@ -69,10 +70,17 @@ if __name__ == '__main__':
     with open('app-user.json', encoding='utf-8') as f:
         users = json.load(f)
     for u in users:
-        at = login(u["xgh"], u["password"])['data']['access_token']
-        fat = requests.utils.dict_from_cookiejar(get_cookies(at))['first_access_token']
-        for i in get_list(fat, "today")['data']:
-            t = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-            if i["status"] is None:
-                m = signin(fat, i["id"], u["longitude"], u["latitude"])["msg"]
-                print(t, "xgh: " + u["xgh"], "id: " + i["id"], m)
+        at = login(u['xgh'], u['password'])['data']['access_token']
+        fat = requests.utils.dict_from_cookiejar(getCookies(at))['first_access_token']
+        us = getUserScheduling(fat)
+        if us['data']['lasterrow'] is None:
+            i = us['data']['scheduling']['id']
+            uci = userCheckIn(fat, i, u['address'], u['info'])
+            t = uci['data']['sign_at']
+            m = uci['msg']
+            print(t, 'xgh: ' + u['xgh'], 'id: ' + i, m)
+        else:
+            i = us['data']['scheduling']['id']
+            t = us['data']['lasterrow']['sign_at']
+            n = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(n, 'xgh: ' + u['xgh'], 'id: ' + i, f'{t}に手動でチェックインしました')
